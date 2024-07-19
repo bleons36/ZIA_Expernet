675eddf195f1186754982776ec5326896093d5b7
Sanguk Lee <sulee@expernet.co.kr>
2024-07-19 10:29:31 +0900

    240719

--- zia.tf
+++ zia.tf
@@ -13,3 +13,25 @@ provider "zia" {
   api_key       = "BoHpT3cZwqsd"
   zia_cloud     = "zscaler"
 }
+
+resource "zia_url_categories" "terraform test" {
+  super_category      = "USER_DEFINED"
+  configured_name     = "MCAS Unsanctioned Apps"
+  description         = "MCAS Unsanctioned Apps"
+  keywords            = ["microsoft"]
+  custom_category     = true
+  type                = "URL_CATEGORY"
+  urls = [
+    ".coupons.com",
+    ".resource.alaskaair.net",
+    ".techrepublic.com",
+    ".dailymotion.com",
+    ".osiriscomm.com",
+    ".uefa.com",
+    ".Logz.io",
+    ".alexa.com",
+    ".baidu.com",
+    ".cnn.com",
+    ".level3.com",
+  ]
+}

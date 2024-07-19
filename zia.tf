adb12eb340622cc488ccafc7b33c3ceac0fbbaab
Sanguk Lee <sulee@expernet.co.kr>
2024-07-19 10:20:05 +0900

    240719

---
+++ zia.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+        zia = {
+            version = "2.7.0"
+            source = "zscaler/zia"
+        }
+    }
+}
+
+provider "zia" {
+  username      = 'admin@lab.expernet.co.kr'
+  password      = 'Zia@1234'
+  api_key       = 'BoHpT3cZwqsd'
+  zia_cloud     = 'zscaler'
+}
